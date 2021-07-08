9a576a583b64c6198d0331cba8e711ebcb0ddf5d
konstantin <konstantin.klein@hochfrequenz.de>
2021-07-08 15:53:13 +0200

    Initial Code + Github Actions (#1)

---
+++ TimeSliceNet/TimeSliceTests/PlainTimeSliceTests.cs
@@ -0,0 +1,25 @@
+using System;
+using NUnit.Framework;
+using TimeSlice;
+
+namespace TimeSliceTests
+{
+    public class PlainTimeSliceTests
+    {
+        [Test]
+        [TestCase("2021-07-01T00:00:00Z", " ", null)]
+        [TestCase("2021-07-01T00:00:00Z", "2021-07-01T00:01:00Z", 60)]
+        public void TestDuration(string startString, string endString, int? expectedDurationSeconds)
+        {
+            var start = DateTimeOffset.Parse(startString);
+            DateTimeOffset? end = string.IsNullOrWhiteSpace(endString) ? null : DateTimeOffset.Parse(endString);
+            var pts = new PlainTimeSlice
+            {
+                Start = start,
+                End = end
+            };
+            var actualDuration = pts.Duration?.TotalSeconds;
+            Assert.AreEqual(actualDuration, expectedDurationSeconds);
+        }
+    }
+}
